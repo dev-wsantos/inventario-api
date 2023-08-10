827524d9b35fe7ee0f6db52c031a28b034fc657f
Wellington Santos <wellington.bezerra.santos@outlook.com>
2023-08-09 22:30:24 -0300

    Criando as Entidades do Domínio

---
+++ InventarioAPI/Models/Categoria.cs
@@ -0,0 +1,9 @@
+﻿namespace InventarioAPI.Models;
+
+public class Categoria
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public bool Situacao { get; set; }
+    public string? ImageUrl { get; set; }
+}
---
+++ InventarioAPI/Models/Produto.cs
@@ -0,0 +1,13 @@
+﻿namespace InventarioAPI.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public bool Situacao { get; set; }
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+}
