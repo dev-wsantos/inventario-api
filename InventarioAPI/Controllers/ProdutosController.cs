b891ac91e88b56e2cef5876b7fb69e176fc897bb
Wellington Bezerra dos Santos <wellington.santos@basis.com.br>
2023-08-10 11:29:32 -0300

    Criando o ProdutoController e os seus métodos

---
+++ InventarioAPI/Controllers/ProdutosController.cs
@@ -0,0 +1,89 @@
+﻿using InventarioAPI.Context;
+using InventarioAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventarioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly InventarioApiDbContext _context;
+
+        public ProdutosController (InventarioApiDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get ()
+        {
+            var produtos = _context.Produtos?.ToList ();
+
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados.");
+            }
+
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name="ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos?.FirstOrDefault(p => p.Id == id);
+            
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado.");
+            }
+
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post (Produto produto) 
+        {
+            if(produto is null)
+            {
+                return BadRequest();
+            }
+
+            _context.Produtos?.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto", new { id = produto.Id }, produto);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Patch(int id, Produto produto)
+        {
+            if (id != produto.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produto).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos?.FirstOrDefault(p => p.Id == id);
+
+            if (produto is null)
+            {
+                return NotFound("Produto não localizado.");
+            }
+
+            _context.Produtos?.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+    }
+}
